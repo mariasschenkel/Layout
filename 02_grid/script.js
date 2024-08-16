d2de07bb4c6bab43c32349adc8b0c2399a547003
Maria Eduarda Strassburger Schenkel <meduarda.schenkek@gmail.com>
2024-08-16 11:09:23 -0300

    Update script.js

--- 02_grid/script.js
+++ 02_grid/script.js
@@ -1,5 +1,10 @@
+let nome=prompt("Digite seu nome");
+alert("Olá "+nome+", tudo bem?");
 //alerta na tela
 alert("Olá mundo!");
 
 //console do navegador
 console.log("Teste de console");
+
+//inclui um html em um elemento identificado pelo ID
+document.getElementById("demo").innerHTML = "Madu Schenkel";
